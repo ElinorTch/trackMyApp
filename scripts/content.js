7b4f97f9dd9ee1121ebba1fa913516565e30f422
ElinorTch <tchamoeliicode@gmail.com>
2025-05-14 15:05:28 +0200

    Feat: begining of the job tracking functionality with content script and update track.html

---
+++ scripts/content.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const trackButton = document.getElementById("track-button");
+  trackButton?.addEventListener("click", function () {
+    console.log("Track button clicked");
+  });
+});
