024377daebbdd4fa4ffeeb2e07e68ead0ed66871
ElinorTch <tchamoeliicode@gmail.com>
2025-05-19 10:59:57 +0200

    Feat: Refactor message handling in background and content scripts; include job URL in message payload for enhanced job tracking

--- background/background.js
+++ background/background.js
@@ -3,9 +3,12 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     const queryParameters = tab.url.split("?")[1];
     const urlParams = new URLSearchParams(queryParameters);
 
-    chrome.tabs.sendMessage(tabId, {
-      type: "job",
-      jobId: urlParams.get("currentJobId"),
-    });
+    if (changeInfo.status === "complete") {
+      chrome.tabs.sendMessage(tabId, {
+        type: "job",
+        jobId: urlParams.get("currentJobId"),
+        jobUrl: tab.url,
+      });
+    }
   }
 });
--- scripts/content.js
+++ scripts/content.js
@@ -1,15 +1,33 @@
-() => {
-  console.log("Content script loaded");
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    const { type, jobId } = message;
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  const { type, jobId, jobUrl } = message;
 
-    if (type === "job") {
-      currentJobId = jobId;
-      getJobDetails();
-    }
-  });
+  if (type === "job") {
+    const jobInfo = getLinkedinJobDetails(jobId, jobUrl);
+    console.log("Job Info:", jobInfo);
+  }
+});
 
-  const getJobDetails = () => {
-    console.log("Fetching job details...");
+const getLinkedinJobDetails = (jobId, jobURL) => {
+  const companyElement = document.querySelector(
+    ".job-details-jobs-unified-top-card__company-name a"
+  );
+  const jobTitleElement = document.querySelector(
+    ".job-details-jobs-unified-top-card__job-title"
+  );
+
+  const companyName = companyElement?.textContent?.trim();
+  const jobTitle = jobTitleElement?.textContent?.trim();
+  const appliedDate = new Date().toISOString();
+  const lastUpdated = new Date().toISOString();
+
+  return {
+    id: jobId,
+    url: jobURL,
+    company: companyName,
+    title: jobTitle,
+    status: "applied",
+    source: "linkedIn",
+    appliedDate,
+    lastUpdated,
   };
 };
