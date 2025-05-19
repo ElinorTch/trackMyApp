ccc7e59be691e5934e6d04db57653c0169a78646
ElinorTch <tchamoeliicode@gmail.com>
2025-05-19 09:21:30 +0200

    Feat: Refactor LinkedIn background script; update manifest for service worker and URL matching; enhance content script for job message handling

--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,15 @@
-(() => {
+() => {
+  console.log("Content script loaded");
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     const { type, jobId } = message;
 
     if (type === "job") {
+      currentJobId = jobId;
+      getJobDetails();
     }
-  })
-});
+  });
+
+  const getJobDetails = () => {
+    console.log("Fetching job details...");
+  };
+};
