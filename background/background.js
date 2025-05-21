a70e237acd288a8745081a3b708346940f7cf7aa
Tchamo Elinor <tchamoelii@gmail.com>
2025-05-21 08:05:32 +0100

    Feat: Implement Google authentication flow compatible with all navigator with token storage and update UI elements

--- background/background.js
+++ background/background.js
@@ -1,3 +1,11 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === "START_AUTH") {
+    chrome.tabs.create({
+      url: "localhost:3001/google/auth",
+    });
+  }
+});
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (tab.url && tab.url.includes("linkedin.com")) {
     const queryParameters = tab.url.split("?")[1];
--- track.js
+++ track.js
@@ -1,3 +1,13 @@
+document.getElementById('login').addEventListener('click', function () {
+  chrome.runtime.sendMessage({ type: "START_AUTH" }, (response) => {
+    if (response?.token) {
+      console.log("Token reçu :", response.token);
+    } else {
+      console.error("Erreur d'auth:", response?.error);
+    }
+  });
+});
+
 window.onload = function () {
   if (localStorage.getItem("accessToken")) {
     document.getElementById('signin-button').style.display = 'none';
